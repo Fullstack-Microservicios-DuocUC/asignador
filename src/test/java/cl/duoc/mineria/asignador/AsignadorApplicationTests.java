e2f6a055e12bb91959f0ab889594b9f4531ca535
zBleend <juancrichile@gmail.com>
2026-06-03 18:52:49 -0400

    Correcion de proyecto

--- src/test/java/cl/duoc/mineria/asignador/AsignadorApplicationTests.java
+++ src/test/java/cl/duoc/mineria/asignador/AsignadorApplicationTests.java
@@ -1,10 +1,10 @@
-package cl.duoc.mineria.usuarios;
+package cl.duoc.mineria.asignador;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UsuariosApplicationTests {
+class AsignadorApplicationTests {
 
 	@Test
 	void contextLoads() {
